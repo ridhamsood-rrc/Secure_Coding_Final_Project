dd0db1253f4a48e6b287300c1b8e7a008c58da75
Ridhamsood12 <soodridham@gmail.com>
2025-10-18 17:35:35 -0500

    feat: Applied strategy to the payment class.

--- payment/payment.py
+++ payment/payment.py
@@ -1,7 +1,24 @@
 """This module defines the Payment class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Ridham Sood"
+__version__ = "1.0.0"
 
 from billing_account.billing_account import BillingAccount
 from payee.payee import Payee
+from patterns.strategy.payment_strategy import PaymentStrategy
+
+class Payment(PaymentStrategy):
+    """"""
+
+    def __init__(self, strategy: PaymentStrategy):
+        """"""
+        
+        if isinstance(strategy, PaymentStrategy):
+            self.__strategy = strategy
+        else:
+            raise ValueError("Invalid Strategy.")
+    
+    def pay_bill(self, account: BillingAccount, payee: Payee, amount: float) -> str:
+        """"""
+
+        return self.__strategy.process_payment(account, payee, amont)
