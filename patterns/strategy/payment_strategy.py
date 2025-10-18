b8ad5a845914dad37fdae1c29bf0d700d0902974
Ridhamsood12 <soodridham@gmail.com>
2025-10-18 14:52:54 -0500

    feat: Added the superclass for the decorator.

--- patterns/strategy/payment_strategy.py
+++ patterns/strategy/payment_strategy.py
@@ -1,4 +1,17 @@
 """This module defines the PaymentStrategy class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Ridham Sood"
+__version__ = "1.0.0"
+
+from abc import ABC, abstractmethod
+from billing_account.billing_account import BillingAccount
+from payee.payee import Payee
+
+class PaymentStrategy(ABC):
+    """"""
+
+    @abstractmethod
+    def process_payment(account: BillingAccount, payee: Payee, amount: float) -> str:
+        """
+        """
+        pass
