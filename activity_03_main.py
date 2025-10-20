cc6781830d4349f6cd4b14160f0debfd47ff2b44
Ridhamsood12 <soodridham@gmail.com>
2025-10-19 20:14:35 -0500

    feat: Added code according to the comments.

--- activity_03_main.py
+++ activity_03_main.py
@@ -2,12 +2,15 @@
 classes.
 """
 
-__author__ = "ACE Faculty"
+__author__ = "Ridham Sood"
 __version__ = "1.0.0"
 __credits__ = ""
 
 from billing_account.billing_account import BillingAccount
 from payee.payee import Payee
+from payment.payment import Payment
+from patterns.strategy.partial_payment_strategy import PartialPaymentStrategy
+from patterns.strategy.penalty_strategy import PenaltyStrategy
 
 def strategy():
     """Demonstrates the use of the classes defined in this activity."""
@@ -26,31 +29,36 @@ def strategy():
 
     # 1. Create a Payment object with a PenaltyStrategy payment 
     # strategy.
-
+    penalty = Payment(PenaltyStrategy())
 
     # 2. Use the Payment object's pay_bill method to pay the ELECTRICITY
     # bill with an amount that does not pay off the entire balance shown 
     # above - print the result of the pay_bill method.
-
+    
+    print(penalty.pay_bill(account, Payee.ELECTRICITY, 150.0))
+    print()
     
     # 3. Create a Payment object with a PartialPaymentStrategy payment 
     # strategy.
-    
+    partial = Payment(PartialPaymentStrategy())    
 
     # 4. Use the Payment object's pay_bill method to pay the TELEPHONE 
     # bill with an amount that does not pay off the entire balance shown
     # above - print the result of the pay_bill method.
-    
+    print(partial.pay_bill(account, Payee.TELEPHONE, 50.00))
+    print()
 
     # 5. Using the Payment object created in step 3, make another 
     # payment for the TELEPHONE bill with an amount that pays off the 
     # remainder of the balance - print the result of the pay_bill 
     # method.
-   
+    print(partial.pay_bill(account, Payee.TELEPHONE, 100.00)) 
+    print()
 
     # 6. Print the BillingAccount object to show the updated balances 
     # for each of the payees.
-    
+    print("Updated Balances:")
+    print(account, "\n")
 
 if __name__ == "__main__":
     strategy()
